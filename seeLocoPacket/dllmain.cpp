3889b5c0612c60e1966cd4a949c26c45644a82c2
눈누 <78786016+nunnu1028@users.noreply.github.com>
2021-10-30 20:48:26 +0900

    Update dllmain.cpp

--- seeLocoPacket/dllmain.cpp
+++ seeLocoPacket/dllmain.cpp
@@ -17,21 +17,45 @@ DWORD hook(LPVOID lpFunction, DWORD returnAddress)
     return dwAddr;
 }
 
+struct LocoPacketHeader {
+    uint32_t packetID;
+    std::string methodName;
+    uint32_t bodySize;
+};
+
+LocoPacketHeader getLocoPacketHeader(char* packetHeader) {
+    uint32_t packetID = *(uint32_t*)packetHeader;
+    char* methodNameChar = new char[11];
+    memcpy(methodNameChar, packetHeader + 6, 11);
+    std::string methodName(methodNameChar);
+    uint32_t bodySize = *(uint32_t*)(packetHeader + 18);
+
+    LocoPacketHeader locoPacketHeader;
+    locoPacketHeader.packetID = packetID;
+    locoPacketHeader.methodName = methodName;
+    locoPacketHeader.bodySize = bodySize;
+
+    return locoPacketHeader;
+}
+
 void printRequestPacket(char* packet)
 {
-    uint32_t bodySize = *(uint32_t*)(packet + 18);
+    LocoPacketHeader packetHeader = getLocoPacketHeader(packet);
 
     std::cout << "[+] requestPacket!" << std::endl;
+    std::cout << "[-] packetID -> " << packetHeader.packetID << std::endl;
+    std::cout << "[-] methodName -> " << packetHeader.methodName << std::endl;
+    std::cout << "[-] bodySize -> " << packetHeader.bodySize << std::endl;
 
-    if (0 >= bodySize) {
-        std::cout << "[+] body -> none" << std::endl;
+    if (0 >= packetHeader.bodySize) {
+        std::cout << "[-] body -> none" << std::endl << std::endl;
     }
     else
     {
-        std::vector<uint8_t> bsonBody(packet + 22, packet + bodySize + 22);
+        std::vector<uint8_t> bsonBody(packet + 22, packet + packetHeader.bodySize + 22);
         nlohmann::json body = nlohmann::json::from_bson(bsonBody);
 
-        std::cout << "[+] body -> " << body.dump() << std::endl << std::endl;
+        std::cout << "[-] body -> " << body.dump() << std::endl << std::endl;
     }
 }
 
@@ -59,7 +83,7 @@ void seeLocoPacketMain()
     requestReturnAddress += processAddress;
     requestCallAddress += processAddress;
     std::cout << "[+] processAddress -> 0x" << std::uppercase << std::hex << processAddress << std::endl;
-    std::cout << "[+] requestReturnAddress -> 0x" << std::uppercase << std::hex << requestReturnAddress << std::endl << std::endl;
+    std::cout << "[+] requestReturnAddress -> 0x" << std::uppercase << std::hex << requestReturnAddress << std::endl;
     std::cout << "[+] requestCallAddress -> 0x" << std::uppercase << std::hex << requestCallAddress << std::endl << std::endl;
 
     hook(requestHookASM, requestReturnAddress);
