f8fa8503910d025b4473b225e7875d8be9ddb50f
눈누 <78786016+nunnu1028@users.noreply.github.com>
2021-11-02 21:24:07 +0900

    Update dllmain.cpp
    + response hook

--- seeLocoPacket/dllmain.cpp
+++ seeLocoPacket/dllmain.cpp
@@ -1,29 +1,29 @@
 #include "pch.h"
-#include <Windows.h>
 #include <iostream>
 #include <nlohmann/json.hpp>
 
-DWORD requestReturnAddress = 0x10762BD; // first of 84 C0 0F 84 ?? ?? ?? ?? 8B 46 38 3B F8 77 3D 01 7E 3C 2B
-DWORD requestCallAddress = 0x1079160; // Address called from the previous of requestReturnAddress
+using json = nlohmann::json;
+using std::vector;
+using std::cout;
+using std::endl;
+
+DWORD requestReturnAddress = 0x10762B4;
+DWORD responseReturnAddress = 0x10793E4;
+
 BYTE jmpByteArray[] = { 0xE9, 0x00, 0x00, 0x00, 0x00 };
 
-DWORD hook(LPVOID lpFunction, DWORD returnAddress)
+std::vector<std::uint8_t> packetBuffer;
+uint32_t packetSize = -1;
+
+struct LocoPacketHeader
 {
-    DWORD dwAddr = returnAddress - 5;
-    DWORD dwCalc = ((DWORD)lpFunction - dwAddr - 5);
-
-    memcpy(&jmpByteArray[1], &dwCalc, 4);
-    WriteProcessMemory(GetCurrentProcess(), (LPVOID)dwAddr, jmpByteArray, sizeof(jmpByteArray), 0);
-    return dwAddr;
-}
-
-struct LocoPacketHeader {
     uint32_t packetID;
     std::string methodName;
     uint32_t bodySize;
 };
 
-LocoPacketHeader getLocoPacketHeader(char* packetHeader) {
+LocoPacketHeader getLocoPacketHeader(char* packetHeader)
+{
     uint32_t packetID = *(uint32_t*)packetHeader;
     char* methodNameChar = new char[11];
     memcpy(methodNameChar, packetHeader + 6, 11);
@@ -38,14 +38,13 @@ LocoPacketHeader getLocoPacketHeader(char* packetHeader) {
     return locoPacketHeader;
 }
 
-void printRequestPacket(char* packet)
+void printPacket(char* packet)
 {
     LocoPacketHeader packetHeader = getLocoPacketHeader(packet);
 
-    std::cout << "[+] requestPacket!" << std::endl;
-    std::cout << "[-] packetID -> " << packetHeader.packetID << std::endl;
-    std::cout << "[-] methodName -> " << packetHeader.methodName << std::endl;
-    std::cout << "[-] bodySize -> " << packetHeader.bodySize << std::endl;
+    std::cout << "[-] packetID -> " << std::dec << packetHeader.packetID << std::endl;
+    std::cout << "[-] methodName -> " << std::dec << packetHeader.methodName << std::endl;
+    std::cout << "[-] bodySize -> " << std::dec << packetHeader.bodySize << std::endl;
 
     if (0 >= packetHeader.bodySize) {
         std::cout << "[-] body -> none" << std::endl << std::endl;
@@ -59,48 +58,116 @@ void printRequestPacket(char* packet)
     }
 }
 
-void __declspec(naked) requestHookASM()
+void printRequest(char* packet)
 {
-    __asm
-    {
+    std::cout << "[+] request!" << std::endl;
+    printPacket(packet);
+}
+
+void printResponse(char* packet, uint32_t size)
+{
+    try {
+        std::vector<std::uint8_t> buffer(packet, packet + size);
+
+        packetBuffer.insert(packetBuffer.end(), buffer.begin(), buffer.end());
+
+        if (packetSize == -1 && packetBuffer.size() >= 22) {
+            LocoPacketHeader packetHeader = getLocoPacketHeader((char*)packetBuffer.data());
+            packetSize = packetHeader.bodySize + 22;
+        }
+
+        if (packetSize != -1 && packetBuffer.size() >= packetSize) {
+            std::cout << "[+] response!" << std::endl;
+            printPacket((char*)packetBuffer.data());
+
+            std::vector<std::uint8_t> newBuffer = std::vector<std::uint8_t>(packetBuffer.begin() + packetSize, packetBuffer.end());
+            int newSize = newBuffer.size();
+
+            packetBuffer.clear();
+            packetSize = -1;
+
+            printResponse((char*)(newBuffer.data()), newSize);
+        }
+    }
+    catch (std::exception& e) {
+        std::cout << "[!] Exception ->" << e.what() << std::endl << std::endl;
+    }
+}
+
+DWORD hook(LPVOID lpFunction, DWORD returnAddress)
+{
+    DWORD dwAddr = returnAddress - 5;
+    DWORD dwCalc = ((DWORD)lpFunction - dwAddr - 5);
+
+    memcpy(&jmpByteArray[1], &dwCalc, 4);
+    WriteProcessMemory(GetCurrentProcess(), (LPVOID)dwAddr, jmpByteArray, sizeof(jmpByteArray), 0);
+    return dwAddr;
+}
+
+void __declspec(naked) requestHookASM() {
+    __asm {
         pushad
 
         mov ebx, [esi + 0x34]
+
         push ebx
-        call printRequestPacket
-        add esp, 4
+        call printRequest
+        add esp, 0x04
 
         popad
 
-        call requestCallAddress
+        cmp eax, edi
+        cmovb edi, eax
+
         jmp requestReturnAddress
     }
 }
 
-void seeLocoPacketMain()
-{
-    DWORD processAddress = (DWORD)GetModuleHandleA("KakaoTalk.exe");
-    requestReturnAddress += processAddress;
-    requestCallAddress += processAddress;
-    std::cout << "[+] processAddress -> 0x" << std::uppercase << std::hex << processAddress << std::endl;
-    std::cout << "[+] requestReturnAddress -> 0x" << std::uppercase << std::hex << requestReturnAddress << std::endl;
-    std::cout << "[+] requestCallAddress -> 0x" << std::uppercase << std::hex << requestCallAddress << std::endl << std::endl;
+void __declspec(naked) responseHookASM() {
+    __asm {
+        pushad
 
-    hook(requestHookASM, requestReturnAddress);
-    std::cout << "[+] hooked request!" << std::endl << std::endl;
+        mov ebx, [ebp + 0x10]
+        add ebx, 0x14
+
+        push edi
+        push ebx
+        call printResponse
+        add esp, 0x8
+
+        popad
+
+        mov eax, [ebp + 0x10]
+        test eax, eax
+
+        jmp responseReturnAddress
+    }
 }
 
-BOOL APIENTRY DllMain( HMODULE hModule,
-                       DWORD  ul_reason_for_call,
-                       LPVOID lpReserved
-                     )
+void Start()
+{
+    DWORD baseAddress = (DWORD)GetModuleHandleA("KakaoTalk.exe");
+    requestReturnAddress += baseAddress;
+    responseReturnAddress += baseAddress;
+    std::cout << "[+] requestReturnAddress -> 0x" << std::nouppercase << std::hex << requestReturnAddress << std::endl;
+    std::cout << "[+] responseReturnAddress -> 0x" << std::nouppercase << std::hex << responseReturnAddress << std::endl;
+    hook(requestHookASM, requestReturnAddress);
+    std::cout << "[+] hooked request!" << std::endl;
+    hook(responseHookASM, responseReturnAddress);
+    std::cout << "[+] hooked response!" << std::endl << std::endl;
+}
+
+BOOL APIENTRY DllMain(HMODULE hModule,
+    DWORD  ul_reason_for_call,
+    LPVOID lpReserved
+)
 {
     switch (ul_reason_for_call)
     {
     case DLL_PROCESS_ATTACH:
         AllocConsole();
         freopen("CON", "w", stdout);
-        CreateThread(NULL, NULL, (LPTHREAD_START_ROUTINE)seeLocoPacketMain, NULL, NULL, NULL);
+        CreateThread(NULL, NULL, (LPTHREAD_START_ROUTINE)Start, NULL, NULL, NULL);
 
     case DLL_THREAD_ATTACH:
     case DLL_THREAD_DETACH:
@@ -109,4 +176,3 @@ BOOL APIENTRY DllMain( HMODULE hModule,
     }
     return TRUE;
 }
-
